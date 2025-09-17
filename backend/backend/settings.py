518bc2e63d84d99ee8cac53a559315d4505ff96d
kbilly <kemboibilly49@gmail.com>
2025-09-17 16:46:07 +0300

    have the dashboard

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path 
 from datetime import timedelta 
-from dotenv import load_dotenv 
+from dotenv import load_dotenv  
 import os 
  
 load_dotenv()
