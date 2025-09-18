4372dc4186552a4c26a886bb53f313bfacd74576
kbilly <kemboibilly49@gmail.com>
2025-09-18 16:12:34 +0300

    HAVE THE .ENV CONTENT TO CONNECT THE DATABASE

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,16 +95,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = { 
-    "default": { 
-        "ENGINE": "django.db.backends.postgresql", 
-        "NAME": os.getenv("DB_NAME"), 
-        "USER": os.getenv("DB_USER"), 
-        "PASSWORD": os.getenv("DB_PWD"), 
-        "HOST": os.getenv("DB_HOST"), 
-        "PORT": os.getenv("DB_PORT"), 
-    } 
-}
+DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB"),
+            "USER": os.getenv("POSTGRES_USER"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+            "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+            "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        }
+    }
 
 
 # Password validation
