3db8921b406df4232546087b5716ced599bad03c
kbilly <kemboibilly49@gmail.com>
2025-09-18 09:34:08 +0300

    have the requirements.txt file

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin 
 from django.urls import path, include 
 from api.views import CreateUserView 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView  
+from rest_framework_simplejwT.views import TokenObtainPairView, TokenRefreshView  
 
  
 urlpatterns = [ 
